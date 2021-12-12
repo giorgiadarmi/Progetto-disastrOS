38e98351f9f4f3715daa086f82792d8d4e8856ed
giorgiadarmi <95757658+giorgiadarmi@users.noreply.github.com>
2021-12-12 16:07:02 +0100

    Delete disastrOS_msgget.c

--- disastrOS_msgget.c
+++
@@ -1,3 +0,0 @@
-msgget(): restituisce l'identificatore della coda di messaggi per una coda 
-    di messaggi appena creata o restituisce gli identificatori per una coda 
-    che esiste con lo stesso valore di chiave.
