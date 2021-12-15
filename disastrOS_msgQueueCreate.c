3015303973569f409bcbecfbea416439671a07d5
giorgiadarmi <darmiento.1711864@studenti.uniroma1.it>
2021-12-15 12:07:55 +0100

    file syscall

---
+++ disastrOS_msgQueueCreate.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "disastrOS.h"
+#include "disastrOS_syscalls.h"
+#include "disastrOS_msgqueue.h"
+#include "disastrOS_descriptor.h"
+
+
+void internal_msgQueueCreate() {
+
+}
---
+++ disastrOS_msgQueueWrite.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "disastrOS.h"
+#include "disastrOS_syscalls.h"
+#include "disastrOS_msgqueue.h"
+#include "disastrOS_descriptor.h"
+
+
+void internal_msgQueueWrite() {
+
+}
