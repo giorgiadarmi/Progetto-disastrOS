2510d7a615c27f81ad3c016c3f479a975e30fccd
giorgiadarmi <darmiento.1711864@studenti.uniroma1.it>
2021-12-17 11:58:28 +0100

    aggiunta syscall open inizio

--- disastrOS_msgQueueOpen.c
+++ disastrOS_msgQueueOpen.c
@@ -10,4 +10,17 @@
 
 void internal_msgQueueOpen() {
 
+    //specifichiamo il nome della coda di messaggi che si vuole aprire
+    const char *name = (const char*) running->syscall_args[0];
+
+	//trova il puntatore della coda di messaggi con il nome richiesto, tramite la funzione findbyname e lo assegna ad un puntatore
+    MsgQueuePtr *mqdesc = MsgQueuePtrList_findByName(&msg_queues_list, name);
+    MsgQueue *mq = mqdesc->msgQueuePtr;
+
+	//errore in caso di coda di messaggi non trovata
+    if (!mq) {
+        running->syscall_retvalue = DSOS_EMQ_NOEXIST;
+        return;
+    }
+
 }
